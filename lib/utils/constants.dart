ad217d6bc8b54973da33dc5cc6a769523c8e075e
sheriffoladejo <sherifffoladejo@gmail.com>
2023-10-20 10:47:26 +0100

    Default commit

--- lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  static const String server_url = "https://www.cacscar-clothes.com";
-  static const String server_get_url = "www.cacscar-clothes.com";
-  static const String api_url = "/api";
-
-  static final String date_format = "MM-dd-yyyy";
-}
